0183a38da841ea6c73b221f3c7aa14779abe4df3
Cédric Lucchese <cedric.lucchese@epitech.eu>
2020-11-03 12:48:28 +0100

    Implementation of setter finished

--- MLStudio/Device.cpp
+++ MLStudio/Device.cpp
@@ -3,3 +3,45 @@
  * @ Description: Device implementation
  */
 
+#include "Device.hpp"
+
+bool Device::setRecord(bool record) noexcept
+{
+    if (record() == record)
+        return false;
+    _data->setRecord(record);
+    return true;
+}
+
+bool setSampleRate(int sampleRate) noexcept
+{
+    if (sampleRate() == sampleRate)
+        return false;
+    _data->setSampleRate(sampleRate);
+    return true;
+}
+
+bool setFormat(const Audio::Device::Format &format) noexcept
+{
+    if (format() == format)
+        return false;
+    _data->setFormat(format);
+    return true;
+}
+
+bool setChannels(uint8 channels) noexcept
+{
+    if (channels() == channels)
+        return false;
+    _data->setChannels(channels);
+    return true;
+}
+
+bool setSample(uint16 sample) noexcept
+{
+    if (sample() == sample)
+        return false;
+    _data->setSample(sample);
+    return true;
+}
+
--- MLStudio/Device.hpp
+++ MLStudio/Device.hpp
@@ -29,31 +29,31 @@ public:
     [[nodiscard]] bool record(void) const noexcept { /* TODO*/ }
 
     /** @brief SET the record */
-    void setRecord(bool record) noexcept;
+    bool setRecord(bool record) noexcept;
 
     /** @brief GET the sample rate */
     [[nodiscard]] int sampleRate(void) const noexcept { return _data->sampleRate(); }
 
     /** @brief SET the sample rate */
-    void setSampleRate(int sampleRate) noexcept;
+    bool setSampleRate(int sampleRate) noexcept;
 
     /** @brief GET the format */
     [[nodiscard]] const Audio::Device::Format &format(void) const noexcept { return _data->format(); }
 
     /** @brief SET the format */
-    void setFormat(const Audio::Device::Format &format) noexcept;
+    bool setFormat(const Audio::Device::Format &format) noexcept;
 
     /** @brief GET the channels */
     [[nodiscard]] uint8 channels(void) const noexcept { return _data->channels(); }
 
     /** @brief SET the channels */
-    void setChannels(uint8 channels) noexcept;
+    bool setChannels(uint8 channels) noexcept;
 
     /** @brief GET the record */
     [[nodiscard]] uin16 sample(void) const noexcept { return _data->sample(); }
 
     /** @brief SET the record */
-    void setSample(uint16 sample) noexcept;
+    bool setSample(uint16 sample) noexcept;
 
 signals:
 
