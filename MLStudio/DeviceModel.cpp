e0d5492a023924048d9645744cdf7235e522b298
Cédric Lucchese <cedric.lucchese@epitech.eu>
2020-11-03 12:57:38 +0100

    DeviceModel implemented

---
+++ MLStudio/DeviceModel.cpp
@@ -0,0 +1,36 @@
+/**
+ * @ Author: Cédric Lucchese
+ * @ Description: Device Model implementation
+ */
+
+#include <stdexcept>
+
+#include "DeviceModel.hpp"
+
+QHash<int, QByteArray> DeviceModel::roleNames(void) const noexcept
+{
+    return QHash<int, QByteArray> {
+        { Roles::Name, "name" },
+        { Roles::IsInput, "isInput" }
+    };
+}
+
+QVariant DeviceModel::data(const QModelIndex &index, int role) const noexcept_ndebug
+{
+    coreAssert(index.row() < 0 || index.row() >= count(),
+        throw std::range_error("InstancesModel::data: Given index is not in range"));
+    const auto &child = (*_data)[index.row()];
+    switch (role) {
+    case Roles::Name:
+        return child.name();
+    case Roles::IsInput:
+        return child.isInput();
+    default:
+        return QVariant();
+    }
+}
+
+DeviceModel::DevicePtr DeviceModel::instantiate(const QString &name)
+{
+    /** TODO */
+}
